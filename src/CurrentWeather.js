6d303448f23bc16319ef91dfedd5c7967e24dc17
Patrik Beichner <patrik.beichner@gmail.com>
2022-02-23 17:53:25 -0500

    CurrentWeather component items and data tests

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit'
 
 export default function CurrentWeather({currentData, hourlyData}) {
-  return (
-    <div>
-        <p>Current weather</p>
-        <p>{currentData.temp}</p>
-    </div>
-  )
+  
+    const iconUrl = 'http://openweathermap.org/img/wn/'
+
+    if (currentData) {
+    
+        let currTemp = Math.round(currentData.temp)
+        console.log(currTemp)
+        let fLike = Math.round(currentData.feels_like)
+
+    return (
+    <>
+        <div>
+            <p>Current weather</p>
+            <p>{currTemp}&deg;F</p>
+            <p>{fLike}&deg;F</p>
+            <p>{currentData.humidity}&#37;</p>
+            <p>{currentData.weather[0].main}</p>
+            <img src={`${iconUrl}${currentData.weather[0].icon}@2x.png`} alt={currentData.weather[0].main}/>
+        </div>
+    </>
+  )} else {
+      return (
+          <div>
+              <h1>Loading...</h1>
+          </div>
+      )
+  }
 }

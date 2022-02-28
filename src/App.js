7587551cd6fb29eff17fb766805929e893a0e75d
Patrik Beichner <patrik.beichner@gmail.com>
2022-02-27 19:16:50 -0500

    Alert component setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import CurrentWeather from './CurrentWeather';
 import { WEATHER } from './shared/weather';
 import DailyWeather from './DailyWeather';
+import Alert from './alert';
 
 function App() {
 
@@ -75,7 +76,7 @@ function App() {
         <p>lat: {lat}</p>
         <p>lon: {lon}</p>
       </div> */}
-      
+        {forecast.alerts ? <Alert alerts={forecast.alerts} /> : null}
         <CurrentWeather currentData={forecast.current} hourlyData={forecast.hourly}/>
         <DailyWeather dailyData={forecast.daily} />
     </div>

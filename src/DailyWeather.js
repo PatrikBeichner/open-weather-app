59392b502dda1cc9672c5e12b17489b4db3fc790
Patrik Beichner <patrik.beichner@gmail.com>
2022-03-16 15:56:39 -0400

    daily weather code cleanup

--- src/DailyWeather.js
+++ src/DailyWeather.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -72,35 +70,5 @@ export default function DailyWeather({ dailyData }) {
   }
 }
 
-{
-  /* <>
-        
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-            <Grid item xs={8}>
-            <Card sx={{ maxWidth: 345, display: 'flex' }}>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={icon}
-                    alt={currentData.weather[0].main} />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {currTemp}&deg;F
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {currentData.weather[0].main}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        feels like: {fLike}&deg;F
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        humidity: {currentData.humidity}&#37;
-                    </Typography>
-                </CardContent>
-            </Card>
-            </Grid>
-            </Grid>
-        </Box>
-    </> */
-}
+
+

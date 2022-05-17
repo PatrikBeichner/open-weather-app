ecfc5027edf072741ed8ca567bfc383c2bb1d6b8
Patrik Beichner <patrik.beichner@gmail.com>
2022-05-16 21:25:33 -0400

    code cleanup in app.js

--- src/App.js
+++ src/App.js
@@ -25,105 +25,29 @@ import {
 } from '@mui/material';
 
 function App() {
-  let weatherUrl = '/api/weather?';
-  let reverseUrl = '/api/reverse?';
-  let geoUrl = '/api/location?';
+  //let weatherUrl = '/api/weather?';
+  //let reverseUrl = '/api/reverse?';
+  //let geoUrl = '/api/location?';
 
-  //const [lat, setLat] = useState();
-  //const [lon, setLon] = useState();
-  //const [city, setCity] = useState();
 
   const [country, setCountry] = useState('US');
   const [zip, setZip] = useState('15201');
   const [location, setLocation] = useState({});
-  let lat = location.lat;
-  let lon = location.lon;
-  let city = location.name;
-
-  //const [forecast, setForecast] = useState([])
 
   //use local data during development
   //const [forecast, setForecast] = useState(WEATHER);
   const [forecast, setForecast] = useState();
 
-  //const [errors, setErrors] = useState([]);
-
-  //const apiKey = process.env.REACT_APP_API_KEY;
-  //const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${apiKey}`;
-  //const apiGeoUrl = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip},${country}&appid=${apiKey}`;
-
-  // async function fetchWeat() {
-  //   if(location) {
-  //     console.log('this is from the thing')
-  //     fetch(`${weatherUrl}latitude=${location.lat}&longitude=${location.lon}`)
-  //     .then((res) => res.json())
-  //     .then((data) => setForecast(data));
-  //   }
-  //   }
 
   const handleCountry = (event) => {
-    //updateCountry = event.target.value;
     setCountry(event.target.value);
     console.log('this is country: ' + country);
   };
   const handleZip = (event) => {
-    //updateZip = event.target.value;
     setZip(event.target.value);
     console.log('this is zip: ' + zip);
   };
 
-  //This is the one you were working on
-  //   useEffect(() => {
-  //     //let newLat = '';
-  //     //let newLon = '';
-  //     const fetchCords = async () => {
-  //       console.log(`/api/location?zip=${zip}&country=${country}`)
-  //     await fetch(`/api/location?zip=${zip}&country=${country}`)
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //          //newLat = data.latitude;
-  //          //newLon = data.longitude;
-  //         //setLocation(data)
-  //       setLat(data.lat)
-  //       setLon(data.lon)}
-  //         );
-  //         console.log(location)
-
-  //   }
-  //     const fetchWeather = async () => {
-  //       console.log(`/api/weather?latitude=${location.lat}&longitude=${location.lon}`)
-  //       await fetch(`/api/weather?latitude=${location.lat}&longitude=${location.lon}`)
-  //       .then((res) => res.json())
-  //       .then((data2) => setForecast(data2));
-  //     }
-  //   fetchCords()
-  //   .catch(console.error);
-
-  //   if ( lat && lon ) {
-  //   fetchWeather()
-  //   .catch(console.error);
-  //   }
-  // },[zip, country, location, lat, lon])
-
-  //   useEffect(() => {
-  //     if (location) {
-  //     const fetchWeather = async () => {
-  //       await fetch(`/api/weather?latitude=${location.lat}&longitude=${location.lon}`, {
-  //         headers : {
-  //         'Content-Type': 'application/json',
-  //         'Accept': 'application/json'
-  //         }
-  //       })
-  //       .then(res=>res.json())
-  //       .then(res=> {
-  //         setForecast(res)
-  //         //console.log(data)
-  //       })
-  //     }
-  //   fetchWeather()
-  //   .catch(console.error);
-  // }
-  //   }, [location])
 
   useEffect(() => {
     const fetchCor = async () => {
@@ -135,10 +59,7 @@ function App() {
         //console.log(loc.location.lat)
         //console.log(location)
 
-        // const wea = await fetch(`/api/weather?latitude=${location.lat}&longitude=${location.lon}`)
-        // .then((res) => res.json())
-        // .then((res) => setForecast(res))
-        //console.log(forecast)
+        
       } catch (e) {
         console.log(e);
       }
@@ -150,22 +71,11 @@ function App() {
     fetch(`/api/weather?latitude=${location.lat}&longitude=${location.lon}`)
       .then((res) => res.json())
       .then((result2) => setForecast(result2));
+      //console.log(forecast)
   };
 
   useEffect(() => {
-    // const fetchWea = async () => {
-    //   try {
-    //     const wea = await fetch(`/api/weather?latitude=${location.lat}&longitude=${location.lon}`)
-    //       .then((res) => res.json())
-    //       .then((res) => setForecast(res));
-    //       //console.log('this is fetchWea')
-    //       //console.log(forecast.daily[0].temp.min)
-    //       //console.log(forecast)
-    //   } catch (e) {
-    //     console.log(e);
-    //   }
-    // };
-    //fetchWea();
+
     if (location.lat && location.lon) {
       searchWeather();
     }
